19e2eafaad3efaa4de6e1c4650767ddf55de775f
Todor Hristov <todorhristov2006@gmail.com>
2025-10-04 22:11:50 +0300

    Initialize expense tracker with basic structure and files

---
+++ expense_tracker/expense_manager.py
@@ -0,0 +1,3 @@
+import csv
+from datetime import datetime
+from expense_tracker.models import Expense
---
+++ expense_tracker/models.py
@@ -0,0 +1,6 @@
+class Expense:
+    def __init_(self, name, amount, category, date):
+        self.name = name
+        self.amount = amount
+        self.category = category
+        self.date = date
